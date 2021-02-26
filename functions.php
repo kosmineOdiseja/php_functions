ff135382db8f597535bb9d303dc268bc13a0be9c
kosmineOdiseja <aliusbosas@gmail.com>
2021-02-26 15:24:56 +0200

    fixed

--- functions.php
+++ functions.php
@@ -333,17 +333,18 @@ var_dump(implode('hello', array())); // string(0)
         echo " and exists at position $pos";
     }
 
-    // Using !==
     $mystring = 'abc';
     $findme   = 'a';
     $pos = strpos($mystring, $findme);
-
+    
     // The !== operator can also be used.  Using != would not work as expected
     // because the position of 'a' is 0. The statement (0 != false) evaluates
     // to false.
     if ($pos !== false) {
-        echo "The string '$findme' was found in the string '$mystring'";
-            echo " and exists at position $pos";
+         echo "The string '$findme' was found in the string '$mystring'";
+             echo " and exists at position $pos";
     } else {
-        echo "The string '$findme' was not found in the string '$mystring'";
+         echo "The string '$findme' was not found in the string '$mystring'";
     }
+}
+?>
