7ae78f1f84772268936756448921e4b9b01e7086
kosmineOdiseja <aliusbosas@gmail.com>
2021-03-05 17:32:21 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -518,5 +518,141 @@ foreach ($values as $value) {
  $b = new B();
  print($b); // Instances of B() can be treated as a strings! 
  print(is_string($b) ? 'true' : 'false' ); 
+ echo "\n";
 
+ /** preg_replace - Perform a regular expression seach and replace
+   * 
+   * Description 
+   *    
+   * preg_replace ( string|array $pattern , string|array $replacement , 
+   * string|array $subject , int $limit = -1 , int &$count = null ) : string|array|null
+   * 
+   * Seaches subject or matches to pattern and replaces them with replacement
+   * 
+   * Parameters 
+   *    Pattern to search for. It can be either a string or an array with string 
+   * 
+   * Several PCRE midifies are also avaible. 
+   * 
+   * Replacement 
+   *    The string or an array with strings to replace. If this parameter 
+   *    is a string and the pattern parameter is an array, all patterns
+   *     will be replaced by that string. If both pattern and replacement
+   *     parameters are arrays, each pattern will be replaced by the replacement 
+   *    counterpart. If there are fewer elements in the replacement array than in 
+   *    the pattern array, any extra patterns will be replaced by an empty string.
+   * 
+   *    replacement may contain references of the form \n or $n, with the latter 
+   *    form being the preferred one. Every such reference will be replaced by the text 
+   *    captured by the n'th parenthesized pattern. n can be from 0 to 99, and \0 or $0
+   *     refers to the text matched by the whole pattern. Opening parentheses are counted 
+   *    from left to right (starting from 1) to obtain the number of the capturing subpattern.
+   *     Note that backslashes in string literals may require to be escaped.
+   *    
+   *     When working with a replacement pattern where a backreference is immediately followed by 
+   *    another number (i.e.: placing a literal number immediately after a matched pattern),
+   *     you cannot use the familiar \1 notation for your backreference. \11, for example, 
+   *    would confuse preg_replace() since it does not know whether you want the \1 backreference followed
+   *     by a literal 1, or the \11 backreference followed by nothing. In this case the solution
+   *     is to use ${1}1. This creates an isolated $1 backreference, leaving the 1 as a literal.
+   *    
+   *     When using the deprecated e modifier, this function escapes some characters (namely ', ", \ and NULL)
+   *     in the strings that replace the backreferences. This is done to ensure that no syntax errors arise 
+   *    from backreference usage with either single or double quotes (e.g. 'strlen(\'$1\')+strlen("$2")').
+   *     Make sure you are aware of PHP's string syntax to know exactly how the interpreted string will look.
+   * 
+   * Subject 
+   *    The string or an array with strings to search and replace.
+   *    
+   *    If subject is an array, then the search and replace is performed on every entry oj subject, 
+   *    and the return value is an array as well. 
+   * 
+   * Limit 
+   *    the maximum possible replacements for each pattern in each subject string. Defaults to -1 (no limit) 
+   *    
+   * Count  
+   *    if specified, this variable will be filled with the nubmer of replacements done.
+   * 
+   * Return values 
+   *    preg_replace() returns an array f the subject parameter is an array, or a string otherwise 
+   *    if matches are found, the new subject will be returned, otherwise subject will be returned unchanged 
+   *    or null if an error occurred.
+   * 
+   * Errors/ Exceptions
+   *    As of PHP 5.5.0 E_DEPRECATED level error is emitted when passing in the "\e" modifier.
+   *     As of PHP 7.0.0 using the "\e" modifier is an error; an E_WARNING is emitted in this case.
+   * 
+  */
+    $string = 'April 15, 2003';
+    $pattern = '/(\w+) (\d+), (\d+)/i';
+    $replacement = '${1}1,$3';
+    echo preg_replace($pattern, $replacement, $string);
+
+    $string = 'The quick brown fox jumps over the lazy dog.';
+    $patterns = array();
+    $patterns[0] = '/quick/';
+    $patterns[1] = '/brown/';
+    $patterns[2] = '/fox/';
+    $replacements = array();
+    $replacements[2] = 'bear';
+    $replacements[1] = 'black';
+    $replacements[0] = 'slow';
+    echo preg_replace($patterns, $replacements, $string);
+
+    ksort($patterns);
+    ksort($replacements);
+    echo preg_replace($patterns, $replacements, $string);
+
+    /**
+     * file_get_contents - Reads entire file into a string
+     * 
+     * Description 
+     *      file_get_contents ( string $filename , bool $use_include_path = false , 
+     *      resource $context = ? , int $offset = 0 , int $maxlen = ? ) : string|false
+     * 
+     *  This function is similar to file(), except that file_get_contents() returns the 
+     *  file in a string, starting at the specified offset up to maxlen bytes. On failure, file_get_contents() will return false.
+     *  
+     *  file_get_contents() is the preferred way to read the contents of a file into a string. 
+     *  It will use memory mapping techniques if supported by your OS to enhance performance
+     *  
+     *  Note: If you're opening a URI with special characters, such as spaces, you need to encode the URI with urlencode().
+     * 
+     *  Parameters 
+     *  Filename name of the file to read. 
+     *  use_include_path
+     *  Note: The FILE_USE_INCLUDE_PATH constant can be used to trigger include path search. 
+     *  This is not possible if strict typing is enabled, since FILE_USE_INCLUDE_PATH is an int. 
+     *  Use true instead.
+     * 
+     *  Context - A valid context resource created with stream_context_create(). 
+     *  If you don't need to use a custom context, you can skip this parameter by null.
+     * 
+     *  offset - the offset where the reading starts on the original stram. negative offsets
+     *  count from the end of the strem
+     * 
+     *  seeking (offset) is not supported with remote files. Attempting to seek on non-local files may work with
+     *  small offsets, but this is unpredictable because it works on the buffered stream.
+     * 
+     *  maxlen - maximum length of data read. The default is to read until end of file is reached. 
+     *  Note that this parameter is applied to the stream processed by the filters. 
+     * 
+     *  Return Values - The function returns the read data or false on failure.
+     *  Warning
+     *   his function may return Boolean false, but may also return a non-Boolean value which 
+     *   evaluates to false. Please read the section on Booleans for more information. Use the ===
+     *   operator for testing the return value of this function.
+     */
+
+    $homepage = file_get_contents('http://www.example.com/');
+    echo $homepage;
+
+    // If strict types are enabled i.e. declare(strict_types=1);
+    $file = file_get_contents('./people.txt', true);
+    // Otherwise
+    $file = file_get_contents('./people.txt', FILE_USE_INCLUDE_PATH);
+
+    // Read 14 characters starting from the 21st character
+$section = file_get_contents('./people.txt', FALSE, NULL, 20, 14);
+var_dump($section);
 ?>
